6d542045815e41946f2b37e2357736f38154cca8
Eric Anderson <eanderson@palantir.com>
2023-05-19 11:20:03 -0400

    Tweak cli run

--- bin/fix-namespace.js
+++ bin/fix-namespace.js
@@ -1,3 +1,4 @@
 #!/usr/bin/env node --enable-source-maps
 
-import "../lib/cli.js";
+import cli from "../lib/cli.js";
+cli();
--- src/cli.ts
+++ src/cli.ts
@@ -6,56 +6,59 @@ import { processPackage } from "./processPackage.js";
 import { createConsoleLogger } from "./createConsoleLogger.js";
 import { batchRun } from "./cli/batch/mainProcess/batchRun.js";
 
-await yargs(hideBin(process.argv))
-  .command(
-    "foo",
-    "describe",
-    (yargs) => {
-      return yargs.options({
-        workspaceDir: {
-          type: "string",
-          demandOption: true,
-        },
-        startPackageName: {
-          type: "string",
-        },
-      });
-    },
-    async (argv) => {
-      await batchRun(argv.workspaceDir, argv.startPackageName);
-    }
-  )
-  .command(
-    "singlePackageRun",
-    "run against a single package no extras",
-    (yargs) => {
-      return yargs.options({
-        packagePath: {
-          type: "string",
-          demandOption: false,
-          defaultDescription: "cwd",
-          default: process.cwd(),
-          alias: "package",
-        },
-        changesDir: { type: "string" },
-        dryRun: { type: "boolean", default: false },
-        logLevel: {
-          choices: ["fatal", "error", "warn", "info", "debug", "trace", "silent"],
-          default: "info",
-        },
-      } as const);
-    },
-    ({ packagePath, logLevel, dryRun }) => {
-      process.chdir(packagePath);
-
-      const logger = createConsoleLogger(logLevel);
-
-      try {
-        processPackage(process.cwd(), { logger, dryRun });
-      } finally {
-        logger.flush();
+export default async function cli() {
+  return await yargs(hideBin(process.argv))
+    .command(
+      "foo",
+      "describe",
+      (yargs) => {
+        return yargs.options({
+          workspaceDir: {
+            type: "string",
+            demandOption: true,
+          },
+          startPackageName: {
+            type: "string",
+          },
+        });
+      },
+      async (argv) => {
+        await batchRun(argv.workspaceDir, argv.startPackageName);
       }
-    }
-  )
-  .strict()
-  .showHelpOnFail(true).argv;
+    )
+    .command(
+      "singlePackageRun",
+      "run against a single package no extras",
+      (yargs) => {
+        return yargs.options({
+          packagePath: {
+            type: "string",
+            demandOption: false,
+            defaultDescription: "cwd",
+            default: process.cwd(),
+            alias: "package",
+          },
+          changesDir: { type: "string" },
+          dryRun: { type: "boolean", default: false },
+          logLevel: {
+            choices: ["fatal", "error", "warn", "info", "debug", "trace", "silent"],
+            default: "info",
+          },
+        } as const);
+      },
+      ({ packagePath, logLevel, dryRun }) => {
+        process.chdir(packagePath);
+
+        const logger = createConsoleLogger(logLevel);
+
+        try {
+          processPackage(process.cwd(), { logger, dryRun });
+        } finally {
+          logger.flush();
+        }
+      }
+    )
+    .strict()
+    .showHelpOnFail(true)
+    .parseAsync();
+}
