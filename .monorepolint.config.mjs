73ef233f768d1a22776152655c31da2770010d45
Eric Anderson <eanderson@palantir.com>
2023-05-26 17:40:43 -0400

    Add mrl

---
+++ .monorepolint.config.mjs
@@ -0,0 +1,55 @@
+// @ts-check
+import {} from "@typescript-eslint/parser";
+
+import {
+  alphabeticalDependencies,
+  alphabeticalScripts,
+  standardTsconfig,
+  packageEntry,
+  packageOrder,
+  packageScript,
+} from "@monorepolint/rules";
+
+/** @type import("@monorepolint/config").Config */
+export default {
+  rules: [
+    packageEntry({
+      options: {
+        entries: {
+          type: "module",
+          exports: {
+            ".": {
+              types: "./lib/index.d.ts",
+              import: "./lib/index.js",
+            },
+          },
+        },
+      },
+    }),
+    packageScript({
+      options: {
+        scripts: {
+          clean: "rm -rf lib tsconfig.tsbuildinfo",
+        },
+      },
+    }),
+    packageOrder({}),
+    standardTsconfig({
+      options: {
+        template: {
+          extends: "../../tsconfig.base.json",
+          compilerOptions: {
+            outDir: "lib",
+            rootDir: "src",
+          },
+          exclude: ["node_modules", "lib"],
+        },
+      },
+    }),
+    // These work best when last!
+    alphabeticalDependencies({
+      includeWorkspaceRoot: true,
+    }),
+    alphabeticalScripts({}),
+  ],
+};
