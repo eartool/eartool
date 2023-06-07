30d44afe929ede08313a379833401630fd5a9e2a
Eric Anderson <eanderson@palantir.com>
2023-06-07 16:06:47 -0400

    Add use case

--- packages/replacements/src/accumulateRenamesForImportedIdentifier.ts
+++ packages/replacements/src/accumulateRenamesForImportedIdentifier.ts
@@ -1,7 +1,8 @@
 import { SyntaxKind, type ImportSpecifier } from "ts-morph";
 import type { PackageExportRename } from "./PackageExportRename.js";
 import { findEntireQualifiedNameTree } from "@eartool/utils";
-import type { Identifier, ExportSpecifier } from "ts-morph";
+import type { ExportSpecifier } from "ts-morph";
+import type { Identifier } from "ts-morph";
 import * as Assert from "assert";
 import type { Replacements } from "./Replacements.js";
 
@@ -39,25 +40,53 @@ export function accumulateRenamesForImportedIdentifier(
       const fullyQualifiedInstance = findEntireQualifiedNameTree(refNode, packageExportRename.from);
       if (!fullyQualifiedInstance) continue;
 
-      if (specifier && alreadyProcessed && !alreadyProcessed.has(packageExportRename)) {
-        alreadyProcessed.add(packageExportRename);
-        if (packageExportRename.to[0] != packageExportRename.from[0]) {
+      if (packageExportRename.toFileOrModule && specifier) {
+        addImportOrExport(
+          replacements,
+          specifier,
+          packageExportRename.to[0],
+          packageExportRename.toFileOrModule
+        );
+      } else {
+        if (
+          specifier &&
+          alreadyProcessed &&
+          !alreadyProcessed.has(packageExportRename) &&
+          packageExportRename.to[0] != packageExportRename.from[0]
+        ) {
+          alreadyProcessed.add(packageExportRename);
           replacements.insertBefore(specifier, `${packageExportRename.to[0]},`);
         }
-      }
-
-      replacements.replaceNode(fullyQualifiedInstance, packageExportRename.to.join("."));
-
-      if (packageExportRename.toFileOrModule) {
-        replacements.replaceNode(
-          identifier
-            .getFirstAncestorByKindOrThrow(
-              maybeExportSpecifier ? SyntaxKind.ExportDeclaration : SyntaxKind.ImportDeclaration
-            )
-            .getModuleSpecifier()!,
-          `"${packageExportRename.toFileOrModule}"`
-        );
+        replacements.replaceNode(fullyQualifiedInstance, packageExportRename.to.join("."));
+        if (packageExportRename.toFileOrModule) {
+          replacements.replaceNode(
+            identifier
+              .getFirstAncestorByKindOrThrow(
+                maybeExportSpecifier ? SyntaxKind.ExportDeclaration : SyntaxKind.ImportDeclaration
+              )
+              .getModuleSpecifier()!,
+            `"${packageExportRename.toFileOrModule}"`
+          );
+        }
       }
     }
   }
+}
+
+function addImportOrExport(
+  replacements: Replacements,
+  specifier: ImportSpecifier | ExportSpecifier,
+  symbolName: string,
+  newModuleSpecifier: string
+) {
+  const keyword = specifier.isKind(SyntaxKind.ExportSpecifier) ? "export" : "import";
+  const decl = specifier.getFirstAncestorByKindOrThrow(
+    specifier.isKind(SyntaxKind.ExportSpecifier)
+      ? SyntaxKind.ExportDeclaration
+      : SyntaxKind.ImportDeclaration
+  );
+
+  replacements.insertBefore(decl, `${keyword} { ${symbolName} } from "${newModuleSpecifier}";\n`);
+  replacements.replaceNode(specifier.getNameNode(), "");
+  replacements.removeNextSiblingIfComma(specifier);
 }
--- packages/replacements/src/addSingleFileReplacementsForRenames.test.ts
+++ packages/replacements/src/addSingleFileReplacementsForRenames.test.ts
@@ -27,6 +27,43 @@ describe(addSingleFileReplacementsForRenames, () => {
       //
 
       export { bar } from "baz";
+      export {} from "./bar";
+      
+
+      //
+      // <//index.ts>
+      //
+
+      "
+    `);
+    //
+  });
+
+  it("renames a reexport keeping the old! ", () => {
+    const { output } = new TestBuilder()
+      .addFile(
+        "/index.ts",
+        `
+          export {bar, foo} from "./bar";
+      `
+      )
+      .performWork(({ replacements, files }) => {
+        addSingleFileReplacementsForRenames(
+          files.get("/index.ts")!,
+          new Map([["./bar", [{ from: ["bar"], to: ["bar"], toFileOrModule: "baz" }]]]),
+          replacements
+        );
+      })
+      .build();
+
+    expect(output).toMatchInlineSnapshot(`
+      "
+      //
+      // </index.ts>
+      //
+
+      export { bar } from "baz";
+      export { foo } from "./bar";
 
 
       //
