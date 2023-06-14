9c27af270ad5ecc2e596124d50563003b0576062
Eric Anderson <eanderson@palantir.com>
2023-06-14 11:17:26 -0400

    Remove dead code

--- packages/command-refactor/src/worker/cleanupMovedFile.ts
+++ packages/command-refactor/src/worker/cleanupMovedFile.ts
@@ -12,28 +12,21 @@ export function cleanupMovedFile(ctx: WorkerPackageContext, sf: SourceFile) {
   ];
 
   for (const decl of decls) {
-    // console.log(decl.getText());
     for (const namedImport of getNamedSpecifiers(decl)) {
-      // console.log(namedImport.getText());
       const rootExportedSymbol = rootFile
         ?.getExportSymbols()
         .find((s) => s.getName() == namedImport.getText());
 
       for (const rootExportedDecl of rootExportedSymbol?.getDeclarations() ?? []) {
-        // console.log(getSimplifiedNodeInfoAsString(rootExportedDecl));
         if (rootExportedDecl.isKind(SyntaxKind.ExportSpecifier)) {
           const newModuleSpecifier = rootExportedDecl
             .getExportDeclaration()
             .getModuleSpecifierValue()!;
 
-          // console.log(newModuleSpecifier);
-
           addImportOrExport(ctx.replacements, namedImport, undefined, newModuleSpecifier, false);
         }
-        // console.log(getSimplifiedNodeInfoAsString(rootExportedDecl));
       }
 
-      // console.log(getSimplifiedNodeInfoAsString(q));
       ctx.replacements.replaceNode(getDeclaration(namedImport), "");
     }
     if (decl.isKind(SyntaxKind.ImportDeclaration) && decl.getDefaultImport()) {
